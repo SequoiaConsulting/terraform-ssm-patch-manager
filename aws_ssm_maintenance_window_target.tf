50cbd2fb23122c4aa4e8b0e52f777d54439d3a5b
Raj <raj.yadav@sequoia.com>
2020-07-28 19:22:37 +0530

    Changed tag name

--- aws_ssm_maintenance_window_target.tf
+++ aws_ssm_maintenance_window_target.tf
@@ -3,7 +3,7 @@ resource "aws_ssm_maintenance_window_target" "patching_window_target" {
   resource_type = "INSTANCE"
 
   targets {
-    key    = "tag:environment"
+    key    = "environment"
     values = [var.environment]
   }
 //  targets {
