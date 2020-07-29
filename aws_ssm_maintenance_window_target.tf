12f94a0b53d447413e0eb540a6ac7ec221561df7
Raj <raj.yadav@sequoia.com>
2020-07-29 21:13:09 +0530

    Modified tags

--- aws_ssm_maintenance_window_target.tf
+++ aws_ssm_maintenance_window_target.tf
@@ -2,14 +2,13 @@ resource "aws_ssm_maintenance_window_target" "patching_window_target" {
   window_id     = aws_ssm_maintenance_window.patching_window.id
   resource_type = "INSTANCE"
 
-//  targets {
-//    key    = "environment"
-//    values = [var.environment]
-//  }
   targets {
-    key    = "InstanceIds"
-    values = ["i-00afa84d926734d0e"]
-
+    key    = "os"
+    values = [ubuntu]
   }
+//  targets {
+//    key    = "InstanceIds"
+//    values = ["i-00afa84d926734d0e"]
+//  }
 }
 
